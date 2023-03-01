265e5055b7dcabfa06e2380b04a257e5eb04cf94
Emiliano Córdoba <e.a.cordoba89@gmail.com>
2023-02-28 22:45:38 -0300

    se agrega flotante

--- Proyecto-CV/JavaScript/minuevojs.js
+++ Proyecto-CV/JavaScript/minuevojs.js
@@ -1,4 +1,10 @@
+function myFunction() {
+    var foto = document.getElementById("contimagen");
+    if (foto.style.display === "none") {
+        foto.style.display = "block";
+    } else {
+        foto.style.display = "none";
+    }
+}
 
-document.getElementById('ocultar_foto').addEventListener('click', function() {
-    document.getElementById('fotocv').style.display= 'none';
-})
+
